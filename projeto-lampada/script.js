b5297103c338eb0d370121b350cd1d86e3ee74a8
Mauriciosouza8989 <72310791+Mauriciosouza8989@users.noreply.github.com>
2021-05-02 21:27:21 -0300

    Add files via upload

---
+++ projeto-lampada/script.js
@@ -0,0 +1,14 @@
+let corpo = document.getElementById('corpo');
+let lamp = document.getElementById('lamp');
+
+function acenda() {
+    lamp.src = "./images/acesa.jpg";
+}
+
+function apaga() {
+    lamp.src = "./images/apagada.jpg";
+}
+
+function quebrar() {
+    lamp.src = "./images/quebrada.jpg";
+}
